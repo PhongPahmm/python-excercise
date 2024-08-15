3a5d1238e39acbf4b8b1e1d39e7daafe86bb7572
PhongPahm <phongphp03@gmail.com>
2024-08-15 11:16:47 +0700

    update ex5

--- tut1/ex5.py
+++ tut1/ex5.py
@@ -1,6 +1,14 @@
+
+def checkTriangle(a, b, c):
+    if a==0 or b==0 or c==0:
+        return False
+    else:
+        if a+b < c and a+c<b and b+c<a:
+            return True
+        else:
+            return False
+
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
-def checkTriangle(a, b, c):
-    if a==0 or b==0 or c==0:
-        return
+print(checkTriangle(a, b, c))
