1bb490e5bc7da55e0f5e1a230729ddfba0bf3120
PhongPahm <phongphp03@gmail.com>
2024-08-15 15:07:01 +0700

    update ex5

--- tut1/ex5.py
+++ tut1/ex5.py
@@ -3,7 +3,7 @@ def checkTriangle(a, b, c):
     if a==0 or b==0 or c==0:
         return False
     else:
-        if a+b < c and a+c<b and b+c<a:
+        if a+b > c and a+c>b and b+c>a:
             return True
         else:
             return False
